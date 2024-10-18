e14ade710716e65cdaa7217d77f3563e4fd8b0bb
tomsalescamargo <tomsalescama@gmail.com>
2024-10-18 17:25:05 -0300

    Alterações na interface com o usuário

--- onebitcode.py
+++ onebitcode.py
@@ -10,12 +10,13 @@ print("Feito por: Tom Sales")
 print()
 print("ATENÇÃO! POR FAVOR SEJA HONESTO, UTILIZE SOMENTE NAS AULAS QUE FORAM REALMENTE PERDIDAS NA TROCA DE PLATAFORMA.")
 print()
-print('MODO DE USO:')
-print('Selecione na plataforma ONEBITCODE, a aula no qual deseja iniciar a marcação, após isso, volte para o script.')
-print('Após pressionar "enter"(próxima instrução), posicione o mouse sobre o botão "Concluir aula" e aguarde 5 segundos para o script capturar a posição correta do botão.')
-print('Caso deseje interromper a automação, deixe seu mouse parado no canto superior esquerdo da tela')
+print('ETAPAS/MODO DE USO DO PROGRAMA:')
+print()
+print('  1. Selecione a Aula: Entre na plataforma da ONEBITCODE e selecione a aula em que deseja iniciar a marcação.')
 print()
 
+print('  2. Identifique a Posição do Botão: Antes de iniciar a automação, é necessário descobrir a posição do botão na tela do seu computador. Pressione a tecla "ENTER" e, em seguida, posicione o mouse sobre o botão "Concluir aula", na página da aula que você acabou de selecionar. Você terá 5 segundos para garantir que o pyautogui capture a posição corretamente.')
+print()
 # Verificação para pressionar Enter
 while True:
     enter_input = input('Pressione a tecla "enter" quando estiver pronto para posicionar o mouse no botão: ')
@@ -34,6 +35,20 @@ button_position = pyautogui.position()
 print(f"Posição do botão capturada: {button_position}")
 print()
 
+
+print()
+print('  3. Defina a Quantidade: Será preciso informar quantas aulas você deseja marcar como vistas. ATENÇÃO! Ao informar a quantidade, inicia-se a automação, e você terá 5 segundos para reabrir o navegador na página correta.')
+print()
+print('**Caso deseje interromper a automação, deixe seu mouse parado no canto superior esquerdo da tela')
+print()
+print()
+
+
+# Captura a posição atual do mouse
+button_position = pyautogui.position()
+print(f"Posição do botão capturada: {button_position}")
+print()
+
 # Armazena a quantidade de aulas que o usuário deseja marcar
 while True:
     classes_input = input('Quantas aulas você quer marcar? ')
@@ -46,6 +61,7 @@ while True:
             print('Por favor, insira um número inteiro, maior que 0.')
             print()
         else:
+            print("INICIANDO A AUTOMAÇÃO, VÁ PARA A PÁGINA DA AULA!")
             # Realiza os cliques
             for i in range(classes):
                 time.sleep(5)  # Delay antes de cada clique
